e57b1112d583a59bd3610e900efefeafb0415423
s22976 <s22976@pjwstk.edu.pl>
2024-04-12 19:01:41 +0200

    Refaktoryzacja w UserServices

---
+++ UserService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private readonly ClientRepository _clientRepository;
+        private readonly UserCreditService _userCreditService;
+
+        public UserService(ClientRepository clientRepository, UserCreditService userCreditService)
+        {
+            _clientRepository = clientRepository;
+            _userCreditService = userCreditService;
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!IsValidUser(firstName, lastName, email, dateOfBirth))
+            {
+                return false;
+            }
+
+            var client = _clientRepository.GetById(clientId);
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else
+            {
+                int creditLimit = _userCreditService.GetCreditLimit(lastName, dateOfBirth);
+                if (client.Type == "ImportantClient")
+                {
+                    creditLimit *= 2;
+                }
+
+                user.HasCreditLimit = true;
+                user.CreditLimit = creditLimit;
+                
+                if (user.CreditLimit < 500)
+                {
+                    return false;
+                }
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private bool IsValidUser(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            return !string.IsNullOrEmpty(firstName) && 
+                   !string.IsNullOrEmpty(lastName) && 
+                   IsValidEmail(email) && 
+                   IsAgeAtLeast(dateOfBirth, 21);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        private bool IsAgeAtLeast(DateTime dateOfBirth, int minimumAge)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) 
+                age--;
+            return age >= minimumAge;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            return new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+        }
+    }
+}
